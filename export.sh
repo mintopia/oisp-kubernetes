a652d9cf37be300574242ce7b8193a1dc365c09b
Jessica Smith <jess@mintopia.net>
2018-03-11 20:34:27 +0100

    Updates with working hbase

--- export.sh
+++ export.sh
@@ -1,5 +1,7 @@
-for n in $(kubectl get -n cloudfest -o=name pvc,configmap,serviceaccount,secret,ingress,service,deployment,statefulset,hpa,job,cronjob)
+cd templates
+for n in $(kubectl get -n cloudfest -o=name pvc,configmap,secret,ingress,service,deployment,statefulset,hpa)
 do
     mkdir -p $(dirname $n)
     kubectl get -n cloudfest -o=yaml --export $n > $n.yaml
 done
+cd ..
