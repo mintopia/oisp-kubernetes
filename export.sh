3ad29b9dd3523520703a8df19ba5cbe80d4a8c11
Jessica Smith <jess@mintopia.net>
2018-03-12 11:03:57 +0100

    Add ingresses

--- export.sh
+++ export.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd templates
-for n in $(kubectl get -n cloudfest -o=name pvc,configmap,secret,ingress,service,deployment,statefulset,hpa)
+for n in $(kubectl get -n cloudfest-1 -o=name pvc,configmap,secret,ingress,service,deployment,statefulset,hpa)
 do
     mkdir -p $(dirname $n)
-    kubectl get -n cloudfest -o=yaml --export $n > $n.yaml
+    kubectl get -n cloudfest-1 -o=yaml --export $n > $n.yaml
 done
 cd ..
