a874b0af7b24938ad5dcfd0755ce5f66499a3973
Jessica Smith <jess@mintopia.net>
2018-03-12 11:29:39 +0100

    Update readme, import and add ingresses

--- import.sh
+++ import.sh
@@ -7,3 +7,4 @@ kubectl create -n $NAMESPACE -f templates/secrets
 kubectl create -n $NAMESPACE -f templates/persistentvolumeclaims
 kubectl create -n $NAMESPACE -f templates/services
 kubectl create -n $NAMESPACE -f templates/deployments
+kubectl create -n $NAMESPACE -f templates/ingresses
